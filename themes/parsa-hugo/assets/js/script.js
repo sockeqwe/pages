4f7c329a53ddf4480c89e431fb86cd0ac27c991c
Hannes <hannes.dorfmann@gmail.com>
2021-01-13 14:49:54 +0100

    slider toggle

--- themes/parsa-hugo/assets/js/script.js
+++ themes/parsa-hugo/assets/js/script.js
@@ -96,7 +96,7 @@
       ]
     });
   }, 1500);
-*/ 
+*/
 
   // article reading time
   /*
@@ -114,3 +114,33 @@
 
 
 })(jQuery);
+
+/**
+ * Theme switching
+ */
+
+// function to set a given theme/color-scheme
+function setTheme(themeName) {
+  localStorage.setItem('theme', themeName);
+  document.body.className = themeName;
+}
+
+// function to toggle between light and dark theme
+function toggleTheme() {
+  if (localStorage.getItem('theme') === 'theme-dark') {
+    setTheme('theme-light');
+  } else {
+    setTheme('theme-dark');
+  }
+}
+
+// Immediately invoked function to set the theme on initial load
+(function () {
+  if (localStorage.getItem('theme') === 'theme-dark') {
+    setTheme('theme-dark');
+    document.getElementById('slider').checked = false;
+  } else {
+    setTheme('theme-light');
+    document.getElementById('slider').checked = true;
+  }
+})();
