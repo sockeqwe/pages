df9444d1ca3939cfce1639caf6254c46dab97299
Hannes <hannes.dorfmann@gmail.com>
2021-01-29 10:55:28 +0100

    CountdownWidget PR

--- content/CountdownWidget.kt
+++ content/CountdownWidget.kt
@@ -13,10 +13,10 @@ fun secondsRemaining(now : Date, endDate : Date){
 fun remaining_time_between_two_consecutive_days_is_86400_seconds(){
   val oneDayInSeconds = 1*24*60*60
   val format = SimpleDateFormat("yyyy-MM-dd")
-	val d1 = format.parse("2012-01-15")
+  val d1 = format.parse("2012-01-15")
   val d2 = format.parse("2012-01-16")
     
-	val remainingSeconds = secondsRemaining(d1, d2)
+  val remainingSeconds = secondsRemaining(d1, d2)
 
   Assert.assertEquals(oneDayInSeconds, remainingSeconds)
 }
